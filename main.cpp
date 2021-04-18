b5584624812e90cd90ee01466129fd3da5f89d7f
Bob(KurakinPV) <pochta@example.com>
2021-04-18 22:04:29 +0300

    code: добавлено произведение a и b

--- main.cpp
+++ main.cpp
@@ -11,4 +11,5 @@ int main()
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n';
     cout << "A + B = " << a + b << '\n' << "A - B = " << a - b << '\n';
+    cout << "A*B= " << a*b<<'\n';
 }
